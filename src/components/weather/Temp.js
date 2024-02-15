4ac73054d799e21f4381af047f216540f636a996
kabin-ux <122675432+kabin-ux@users.noreply.github.com>
2024-02-15 14:02:23 +0545

    first commit

---
+++ src/components/weather/Temp.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import WeatherCards from './WeatherCards';
+import './style.css';
+
+const Temp = () => {
+    const [searchValue, setSearchValue] = useState('Kathmandu');
+    const [tempInfo, setTempInfo] = useState('');
+
+    const getWeatherInfo = async () => {
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=9e71080fa6ad4fbd5d99820d806f3f41`;
+            
+            let response = await fetch(url);
+            let data = await response.json();
+
+            const {temp, humidity, pressure} = data.main
+            const {country, sunset} = data.sys;
+            const {main: weatherType} = data.weather[0];
+            const {speed} = data.wind;
+            const {name} = data;
+
+            const myNewWeatherInfo = {
+                temp, humidity, pressure, country, sunset, weatherType, speed, name
+            }
+
+            setTempInfo(myNewWeatherInfo);
+
+        }catch(error){
+            console.log("error");
+        }
+    }
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []);
+
+    return (
+        <>
+            <div className='wrap'>
+                <div className='search'>
+                    <input type='search' placeholder='Search...' className='searchTerm' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                    <button className='search-btn' onClick={getWeatherInfo}>Search</button>
+                </div>
+            </div>
+
+            {/*  Main components for weather card*/}
+            <WeatherCards tempInfo={tempInfo} />
+        </>
+    )
+}
+
+export default Temp;
---
+++ src/components/weather/WeatherCards.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react'
+
+const WeatherCards = ({tempInfo}) => {
+    const {
+        temp, humidity, pressure, country, sunset, weatherType, speed, name
+    } = tempInfo;
+
+    const [weatherState, setWeatherState] = useState('');
+
+    useEffect(() => {
+        if(weatherType){
+            switch(weatherType){
+                case "Clouds":
+                    setWeatherState('wi-day-cloudy-high');
+                    break;
+                case "Haze":
+                    setWeatherState('wi-day-haze');
+                    break;
+                case "Clear":
+                    setWeatherState('wi-day-sunny');
+                    break;
+                case "Mist":
+                    setWeatherState('wi-dust');
+                    break;
+                case "Rain":
+                    setWeatherState('wi-rain');
+                    break;
+                case "Snow":
+                    setWeatherState('wi-day-snow');
+                    break;
+                default:
+                    setWeatherState('wi-day-sunny') 
+            };
+        };
+    }, [weatherType]);
+
+    let sec = sunset;
+    let date = new Date(sec * 1000);
+    let sunsettime = `${date.getHours()}:${date.getMinutes()} `;
+
+    return (
+        <>
+            <div className='widget'>
+                <div className='weather-icon'>
+                    <i className={`wi ${weatherState}`}></i>
+                </div>
+
+                <div className='weather-info'>
+                    <div className='temperature'>
+                        <span>{temp}&deg;C</span>
+                    </div>
+                    <div className='description'>
+                        <div className='weather-condition'>{weatherType}</div>
+                        <div className='place'>
+                            {name}, {country}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='date'>{new Date().toLocaleString()}</div>
+
+                    {/* four column components */}
+                <div className='extra-temp'>
+                    <div className='temp-info-minmax'>
+                        <div className='two-sided-section'>
+                            <p>
+                                <i className={'wi wi-sunset'}></i>
+                            </p>
+                            <p  className='extra-info-leftside'>{sunsettime} <br/>
+                            Sunset
+                            </p>
+                            
+                        </div>
+                        <div className='two-sided-section'>
+                            <p>
+                                <i className={'wi wi-strong-wind'}></i>
+                            </p>
+                            <p className='extra-info-leftside'>{speed} <br/>
+                                Speed
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='weather-extra-info'>
+                        <div className='two-sided-section'>
+                            <p>
+                                <i className={'wi wi-rain'}></i>
+                            </p>
+                            <p className='extra-info-leftside'>{pressure} <br/>
+                            Pressure
+                            </p>
+                        </div>
+
+                        <div className='two-sided-section'>
+                            <p>
+                                <i className={'wi wi-humidity'}></i>
+                            </p>
+                            <p className='extra-info-leftside'>{humidity}<br/>
+                            Humidity
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WeatherCards;
